80550dc8da0c96a8ed9edd896fcb0b5a050a64d0
Jammu University's Institute of Engineering and Technology <itcell.uietju@gmail.com>
2024-07-17 20:21:30 +0530

    Add files via upload
    Contains the connection details

---
+++ db-connect.php
@@ -0,0 +1,7 @@
+<?php
+$server = "srv502.hstgr.io";
+$database = "u553562085_fkSpn";
+$dbUserName = "u553562085_rw5Fs";
+$dbUserPwd = 'ZZePdScxvt';
+$conn = mysqli_connect($server, $dbUserName, $dbUserPwd, $database);
+// $conn = mysqli_connect("mysql", "u553562085_rw5Fs", "ZZePdScxvt", "u553562085_fkSpn");
