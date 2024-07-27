4ea02fc500d3803eff6971cc3296f62b0f9650d9
AdMish15 <anadimishra1924@gmail.com>
2024-07-27 23:09:57 +0530

    Minor Patch- Fixed coding error that rendered incorrect social media icons

--- faculty.php
+++ faculty.php
@@ -1,6 +1,6 @@
 <?php
 // for main site production
-require '../../DB/db-connect.php';
+require './../DB/db-connect.php';
 
 if (!$conn) {
   die("Connection Failed! " . mysqli_connect_error());
@@ -32,16 +32,18 @@ mysqli_close($conn);
   </script>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <meta name="description" content=" The University Institute of Engineering & Technology (UIET) was established by the University of Jammu in 2017 as a ‘Centre of Excellence to provide quality education in Engineering & Technology." />
+  <meta name="description"
+    content=" The University Institute of Engineering & Technology (UIET) was established by the University of Jammu in 2017 as a ‘Centre of Excellence to provide quality education in Engineering & Technology." />
   <meta http-equiv="X-UA-Compatible" content="ie=edge" />
   <title>
-    Faculty - Jammu University's Institute of Engineering and Technology, Kathua
+    Faculty - University's Institute of Engineering and Technology, Kathua
   </title>
   <link rel="icon" href="./assets/png/uiet_header.png" />
   <link rel="stylesheet" href="css/styles.css<?php echo "?t=" . time(); ?>" />
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-  <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600;700;900&display=swap" rel="stylesheet" />
+  <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600;700;900&display=swap"
+    rel="stylesheet" />
   <script src="https://kit.fontawesome.com/ed322df7d9.js" crossorigin="anonymous"></script>
 </head>
 
@@ -57,46 +59,66 @@ mysqli_close($conn);
         <?php
         if (!empty($faculty_row))
           foreach ($faculty_row as $rows) {
-        ?>
-          <div class="uiet--faculty-card" style="background-image: linear-gradient(rgba(0,0,0,0),rgba(0,0,0,0.8)), url(<?php echo "https://lh3.googleusercontent.com/d/" . explode("id=", $rows['photo'])[1] ?>);">
-            <div class="uiet--faculty-info">
-              <h4>
-                <?php echo $rows['name']; ?>
-              </h4>
-              <span>
-                <?php echo $rows['role'] . " - " . $rows['department']; ?>
-              </span>
-              <p class="uiet--faculty-qualifications">
-                <?php echo $rows['qualifications']; ?>
-              </p>
-              <p class="uiet--faculty-bio">
-                <?php echo $rows['bio']; ?>
-              </p>
-              <div class="faculty-social-media-handles">
-                <!-- <?php //echo "<a href=\"tel:$rows[phone]\"><i class=\"fa-solid fa-phone\" style=\"color: #11d531;\"></i></a>"; 
+            ?>
+            <div class="uiet--faculty-card"
+              style="background-image: linear-gradient(rgba(0,0,0,0),rgba(0,0,0,0.8)), url(<?php echo "https://lh3.googleusercontent.com/d/" . explode("id=", $rows['photo'])[1] ?>);">
+              <div class="uiet--faculty-info">
+                <h4>
+                  <?php echo $rows['name']; ?>
+                </h4>
+                <span>
+                  <?php echo $rows['role'] . " - " . $rows['department']; ?>
+                </span>
+                <p class="uiet--faculty-qualifications">
+                  <?php echo $rows['qualifications']; ?>
+                </p>
+                <p class="uiet--faculty-bio">
+                  <?php echo $rows['bio']; ?>
+                </p>
+                <div class="faculty-social-media-handles">
+                  <!-- <?php //echo "<a href=\"tel:$rows[phone]\"><i class=\"fa-solid fa-phone\" style=\"color: #11d531;\"></i></a>"; 
                       ?> -->
-                <?php echo "<a href=\"mailto:$rows[email]\"><i class=\"fa-solid fa-envelope\" style=\"color: #dd8e1c;\"></i></a>"; ?>
-                <?php
-                if ($rows["linkedinUrl"] != null && !str_contains($rows["linkedinUrl"], "NULL")) {
-                  if (!str_contains($rows['linkedinUrl'], 'http')) {
-                    $linkedinUrl = "https://" . $rows['linkedinUrl'];
-                    echo "<a href=\"$linkedinUrl\" target=\"_blank\"><i class=\"fa-brands fa-linkedin\" style=\"color: #004aff;\"></i></a>";
-                  } else {
-                    echo "<a href=\"$rows[linkedinUrl]\" target=\"_blank\"><i class=\"fa-brands fa-linkedin\" style=\"color: #004aff;\"></i></a>";
+                  <?php echo "<a href=\"mailto:$rows[email]\"><i class=\"fa-solid fa-envelope\" style=\"color: #dd8e1c;\"></i></a>"; ?>
+                  <?php
+                  if ($rows["linkedinUrl"] != null && !str_contains($rows["linkedinUrl"], "NULL")) {
+                    $socialUrl = !str_contains($rows['linkedinUrl'], 'http') ? "https://" . $rows['linkedinUrl'] : $rows['linkedinUrl'];
+
+                    $icon = '';
+                    $color = '';
+
+                    if (str_contains(strtolower($socialUrl), 'linkedin')) {
+                      $icon = 'fa-linkedin';
+                      $color = '#0A66C2';
+                    } elseif (str_contains(strtolower($socialUrl), 'scholar.google')) {
+                      $icon = 'fa-google';
+                      $color = '#4285F4';
+                    } elseif (str_contains(strtolower($socialUrl), 'facebook')) {
+                      $icon = 'fa-facebook';
+                      $color = '#1877F2';
+                    } elseif (str_contains(strtolower($socialUrl), 'twitter')) {
+                      $icon = 'fa-twitter';
+                      $color = '#1DA1F2';
+                    } else {
+                      // Default to a generic link icon if the platform is not recognized
+                      $icon = 'fa-link';
+                      $color = '#000000';
+                    }
+
+                    echo "<a href=\"$socialUrl\" target=\"_blank\"><i class=\"fa-brands $icon\" style=\"color: $color;\"></i></a>";
                   }
-                }
-                ?>
-                <?php echo "<a href=\"$rows[CVURL]\" target=\"_blank\"><i class=\"fa-solid fa-file-pdf\" style=\"color: #f40;\"></i></a>"; ?>
+                  ?>
+                  <?php echo "<a href=\"$rows[CVURL]\" target=\"_blank\"><i class=\"fa-solid fa-file-pdf\" style=\"color: #f40;\"></i></a>"; ?>
+                </div>
               </div>
             </div>
-          </div>
-        <?php } ?>
+          <?php } ?>
         <!-- Grid Ends -->
       </div>
     </div>
   </section>
   <div class="disclaimer">
-    <p>Due to Administrative reasons entire staff at UIET is engaged at Annual Academic Arrangement basis, <a id="exams" href="https://drive.google.com/file/d/1Ej44pR_8y65LI9jSKxv9Q2S_vXwKNUXT/view?usp=sharing">more on this...📃</a>
+    <p>Due to Administrative reasons entire staff at UIET is engaged at Annual Academic Arrangement basis, <a id="exams"
+        href="https://drive.google.com/file/d/1Ej44pR_8y65LI9jSKxv9Q2S_vXwKNUXT/view?usp=sharing">more on this...📃</a>
     </p>
   </div>
   <?php require_once "components/footer.php" ?>
--- leadership.php
+++ leadership.php
@@ -1,6 +1,6 @@
 <?php
 // for main site production
-require '../../DB/db-connect.php';
+require './../DB/db-connect.php';
 
 if (!$conn) {
     die("Connection Failed! " . mysqli_connect_error());
@@ -32,7 +32,8 @@ mysqli_close($conn);
     </script>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="description" content=" The University Institute of Engineering & Technology (UIET) was established by the University of Jammu in 2017 as a ‘Centre of Excellence to provide quality education in Engineering & Technology." />
+    <meta name="description"
+        content=" The University Institute of Engineering & Technology (UIET) was established by the University of Jammu in 2017 as a ‘Centre of Excellence to provide quality education in Engineering & Technology." />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>
         Leadership - Jammu University's Institute of Engineering and Technology, Kathua
@@ -41,7 +42,8 @@ mysqli_close($conn);
     <link rel="stylesheet" href="css/styles.css<?php echo "?t=" . time(); ?>" />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600;700;900&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600;700;900&display=swap"
+        rel="stylesheet" />
     <script src="https://kit.fontawesome.com/ed322df7d9.js" crossorigin="anonymous"></script>
 </head>
 
@@ -57,43 +59,65 @@ mysqli_close($conn);
                 <?php
                 if (!empty($faculty_row))
                     foreach ($faculty_row as $rows) {
-                ?>
-                    <div class="uiet-leadership--card">
-                        <div class="uiet-leadership-card--img">
-                            <img src="<?php echo "https://lh3.googleusercontent.com/d/" . explode("id=", $rows['Picture'])[1] ?>" alt="">
-                        </div>
-                        <div class="uiet-leadership-card--info">
-                            <div class="uiet-leadership-subbox">
-                                <h2 class="uiet-leadership-info--title">
-                                    <?php echo $rows['Name'] ?>
-                                </h2>
-                                <p class="uiet-leadership-info--name">
-                                    <?php echo $rows['Designation']; ?>
-                                </p>
-                                <br>
-                                <q class="uiet-leadership-info--desc">
-                                    <?php echo $rows['Message']; ?>
-                                </q>
-                                <div class="uiet-leadership--contactbox">
-                                    <?php echo "<a href=\"mailto:$rows[Email]\"><i class=\"fa-solid fa-envelope\" style=\"color: #3e7eb5;\"></i></a>"; ?>
-                                    <?php echo "<a href=\"tel:$rows[PhNumber]\"><i class=\"fa-solid fa-phone\" style=\"color: #22db1f;\"></i></a>"; ?>
-                                    <?php echo "<a href=\"$rows[Profilelink]\" target=\"_blank\"><i class=\"fa-solid fa-file-pdf\" style=\"color: #ec4f27;\"></i></a>"; ?>
-                                    <?php
-                                    if ($rows["Sociallink"] != null && !str_contains($rows["Sociallink"], "NA")) {
-                                        if (!str_contains($rows['Sociallink'], 'http')) {
-                                            $socialUrl = "https://" . $rows['Sociallink'];
-                                            echo "<a href=\"$socialUrl\" target=\"_blank\"><i class=\"fa-brands fa-twitter\" style=\"color: #004aff;\"></i></a>";
-                                        } else {
-                                            echo "<a href=\"$rows[Sociallink]\" target=\"_blank\"><i class=\"fa-brands fa-twitter\" style=\"color: #004aff;\"></i></a>";
-                                        }
-                                    }
-                                    ?>
-                                </div>
+                        ?>
+                        <div class="uiet-leadership--card">
+                            <div class="uiet-leadership-card--img">
+                                <img src="<?php echo "https://lh3.googleusercontent.com/d/" . explode("id=", $rows['Picture'])[1] ?>"
+                                    alt="">
                             </div>
+                            <div class="uiet-leadership-card--info">
+                                <div class="uiet-leadership-subbox">
+                                    <h2 class="uiet-leadership-info--title">
+                                        <?php echo $rows['Name'] ?>
+                                    </h2>
+                                    <p class="uiet-leadership-info--name">
+                                        <?php echo $rows['Designation']; ?>
+                                    </p>
+                                    <br>
+                                    <q class="uiet-leadership-info--desc">
+                                        <?php echo $rows['Message']; ?>
+                                    </q>
+                                    <div class="uiet-leadership--contactbox">
+                                        <?php echo "<a href=\"mailto:$rows[Email]\"><i class=\"fa-solid fa-envelope\" style=\"color: #3e7eb5;\"></i></a>"; ?>
+                                        <?php echo "<a href=\"tel:$rows[PhNumber]\"><i class=\"fa-solid fa-phone\" style=\"color: #22db1f;\"></i></a>"; ?>
+                                        <?php echo "<a href=\"$rows[Profilelink]\" target=\"_blank\"><i class=\"fa-solid fa-file-pdf\" style=\"color: #ec4f27;\"></i></a>"; ?>
+                                        <?php
+                                        if ($rows["Sociallink"] != null && !str_contains($rows["Sociallink"], "NA")) {
+                                            $socialUrl = !str_contains($rows['Sociallink'], 'http') ? "https://" . $rows['Sociallink'] : $rows['Sociallink'];
 
+                                            $icon = '';
+                                            $color = '';
+
+                                            if (str_contains(strtolower($socialUrl), 'linkedin')) {
+                                                $icon = 'fa-linkedin';
+                                                $color = '#0A66C2';
+                                            } elseif (str_contains(strtolower($socialUrl), 'scholar.google')) {
+                                                $icon = 'fa-google-scholar';
+                                                $color = '#4285F4';
+                                            } elseif (str_contains(strtolower($socialUrl), 'facebook')) {
+                                                $icon = 'fa-facebook';
+                                                $color = '#1877F2';
+                                            } elseif (str_contains(strtolower($socialUrl), 'instagram')) {
+                                                $icon = 'fa-instagram';
+                                                $color = '#1DA1F2';
+                                            } elseif (str_contains(strtolower($socialUrl), 'twitter')) {
+                                                $icon = 'fa-twitter';
+                                                $color = '#1DA1F2';
+                                            } else {
+                                                // Default to a generic link icon if the platform is not recognized
+                                                $icon = 'fa-link';
+                                                $color = '#000000';
+                                            }
+
+                                            echo "<a href=\"$socialUrl\" target=\"_blank\"><i class=\"fa-brands $icon\" style=\"color: $color;\"></i></a>";
+                                        }
+                                        ?>
+                                    </div>
+                                </div>
+
+                            </div>
                         </div>
-                    </div>
-                <?php } ?>
+                    <?php } ?>
                 <!-- FlexBox Ends -->
             </div>
         </div>
