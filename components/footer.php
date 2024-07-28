0c140ce280ffd20124ad30c090f84b96b88eca80
AdMish15 <anadimishra1924@gmail.com>
2024-07-28 22:03:18 +0530

    Major Patch- Reworked the enitre footer, removed vaguely named classes and improved overall aesthetics

--- components/footer.php
+++ components/footer.php
@@ -1,73 +1,81 @@
 <footer class="main-footer" id="footer">
   <div class="main-container">
-    <h2 class="uiet-footer-nav-links-title">Navigation</h2>
-    <div class="uiet-footer-nav-links uiet--grid-cols-4">
-      <a href="../index.php">Home</a>
-      <a href="../courses.php">Programs</a>
-      <a href="./index.php#admission">Admissions </a>
-      <a href="../campus.php">Campus</a>
-      <a href="../leadership.php">Leadership</a>
-      <a href="../faculty.php">Faculty</a>
-      <a href="../placements.php">Placements</a>
-      <a href="../committees.php">Committees</a>
-      <a href="../document.php">Documents</a>
-      <a href="../results.php">Results</a>
-      <a href="https://sujammu.samarth.ac.in/index.php/pgportal/grievance-public/public">uGRAMS(Grievances)</a>
-      <a href="../contact.php">Reach us!</a>
-      <a href="https://unijammuopac.libsys10.in">eLibrary</a>
-      <a href="https://jammuuniversity.ac.in">Univeristy of Jammu(Uoj)</a>
-      <a href="https://i4c.mha.gov.in/newsletter-manual.aspx">Cyber Security</a>
-    </div>
-    <div class="main-footer__upper">
-      <div class="main-footer__row main-footer__row-1">
-        <h2 class="heading heading-sm main-footer__heading-sm">
-          <span>Connect @UIET</span>
-        </h2>
-        <div class="main-footer__social-cont">
-          <a target="_blank" rel="noreferrer" href="https://in.linkedin.com/in/university-institute-of-engineering-and-technology-kathua-campus-university-of-jammu-b2a7b2240">
-            <img class="main-footer__icon" src="./assets/png/linkedin-ico.png" alt="icon" />
-          </a>
-          <a target="_blank" rel="noreferrer" href="https://www.youtube.com/@uietkathua/featured">
-            <img class="main-footer__icon" src="./assets/png/yt-ico.png" alt="icon" />
-          </a>
-          <a target="_blank" rel="noreferrer" href="https://www.instagram.com/uietkathua/">
-            <img class="main-footer__icon main-footer__icon--mr-none" src="./assets/png/insta-ico.png" alt="icon" />
-          </a>
-          <a target="_blank" style="padding-left: 6px" rel="noreferrer" href="https://www.facebook.com/UIETKATHUACAMPUS/">
-            <img class="main-footer__icon main-footer__icon--mr-none" src="./assets/png/fb-icon.png" alt="icon" />
-          </a>
-          <script src="https://infinitescollanimations.netlify.app/animations.js<?php echo "?t=" . time(); ?>"></script>
-          <a class="github-profile" href="https://github.com/UIETJU" target="_blank">
-            <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="GitHub icon" style="vertical-align:baseline; width: 28px; border-radius:50%;">
-          </a>
-
-          <a target="_blank" style="padding-left: 6px" rel="noreferrer" href="https://twitter.com/uietkathua">
-            <img class="main-footer__icon main-footer__icon--mr-none" src="./assets/png/twitter-ico.png" alt="icon" />
-          </a>
+    <div class="footer-nav">
+      <h2 class="footer-nav-title">Navigation</h2>
+      <div class="footer-nav-links">
+        <div class="nav-column">
+          <h3>Reach Us</h3>
+          <a href="../contact.php">Contact</a>
+          <a href="https://sujammu.samarth.ac.in/index.php/pgportal/grievance-public/public">Grievances</a>
+          <a href="../results.php">Results</a>
+        </div>
+        <div class="nav-column">
+          <h3>Academics</h3>
+          <a href="../courses.php">Programs</a>
+          <a href="./index.php#admission">Admissions</a>
+          <a href="../document.php">Documents</a>
+          <a href="https://unijammuopac.libsys10.in">eLibrary</a>
+        </div>
+        <div class="nav-column">
+          <h3>About</h3>
+          <a href="../campus.php">Campus</a>
+          <a href="../leadership.php">Leadership</a>
+          <a href="../faculty.php">Faculty</a>
+          <a href="../placements.php">Placements</a>
+          <a href="../committees.php">Committees</a>
+          <a href="https://jammuuniversity.ac.in">University of Jammu (UoJ)</a>
+          <a href="https://i4c.mha.gov.in/newsletter-manual.aspx">Cyber Security</a>
         </div>
       </div>
-      <div class="main-footer__row main-footer__row-2">
-        <h2 class="heading heading-sm text-lt"></h2>
-        <p style="font-size: 20px">
-        <i class="fa-solid fa-phone"></i> 
-          <a class="uiet--footer-contact--link" href="tel:01922291102">01922291102</a>
+    </div>
+
+    <div class="footer-upper">
+      <div class="footer-row footer-logo">
+        <img src="assets/png/uiet_header.png" alt="Institution Logo">
+      </div>
+      <div class="footer-row footer-contact">
+        <p>
+          <i class="fa-solid fa-phone"></i>
+          <a href="tel:01922291102">01922291102</a>
         </p>
-        <p style="font-size: 20px">
-        <i class="fa-solid fa-at"></i>
-          <a class="uiet--footer-contact--link" href="mailto:uietkc@gmail.com">uietkc@gmail.com</a>
+        <p>
+          <i class="fa-solid fa-at"></i>
+          <a href="mailto:uietkc@gmail.com">uietkc@gmail.com</a>
         </p>
-        <p style="font-size: 20px">
-        <i class="fa-solid fa-globe"></i>
-          <a class="uiet--footer-contact--link" href="mailto:itcell.uietju@gmail.com">Web Master</a>
+        <p>
+          <i class="fa-solid fa-globe"></i>
+          <a href="mailto:itcell.uietju@gmail.com">Web Master</a>
         </p>
       </div>
     </div>
 
-    <div class="main-footer__lower">
-      <strong style="font-size: 1.2em">
-        Copyright
-        <?php echo date("Y"); ?> &copy; IT TaskForce, UIET Kathua
-      </strong>
+    <div class="footer-social">
+      <h2>Connect @UIET</h2>
+      <div class="social-icons">
+        <a target="_blank"
+          href="https://in.linkedin.com/in/university-institute-of-engineering-and-technology-kathua-campus-university-of-jammu-b2a7b2240">
+          <i class="fa-brands fa-linkedin"></i>
+        </a>
+        <a target="_blank" href="https://www.youtube.com/@uietkathua/featured">
+          <i class="fa-brands fa-youtube"></i>
+        </a>
+        <a target="_blank" href="https://www.instagram.com/uietkathua/">
+          <i class="fa-brands fa-instagram"></i>
+        </a>
+        <a target="_blank" href="https://www.facebook.com/UIETKATHUACAMPUS/">
+          <i class="fa-brands fa-facebook"></i>
+        </a>
+        <a target="_blank" href="https://github.com/UIETJU">
+          <i class="fa-brands fa-github"></i>
+        </a>
+        <a target="_blank" href="https://twitter.com/uietkathua">
+          <i class="fa-brands fa-twitter"></i>
+        </a>
+      </div>
+    </div>
+
+    <div class="footer-lower">
+      <strong>&copy; <?php echo date("Y"); ?> IT TaskForce, UIET Kathua</strong>
     </div>
   </div>
 </footer>
