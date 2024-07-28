96246e87de270be9a168023df9b73b879cfb0651
AdMish15 <anadimishra1924@gmail.com>
2024-07-28 23:33:20 +0530

    Fix-Footer Rework

--- components/footer.php
+++ components/footer.php
@@ -1,30 +1,36 @@
 <footer class="main-footer" id="footer">
   <div class="main-container">
     <div class="footer-nav">
-      <h2 class="footer-nav-title">Navigation</h2>
+      <h2 class="footer-nav-title"></h2>
       <div class="footer-nav-links">
         <div class="nav-column">
           <h3>Reach Us</h3>
-          <a href="../contact.php">Contact</a>
-          <a href="https://sujammu.samarth.ac.in/index.php/pgportal/grievance-public/public">Grievances</a>
-          <a href="../results.php">Results</a>
+          <a href="./index.php#admission"><i class="fa-solid fa-door-open"></i> Admissions</a>
+          <a href="../contact.php"><i class="fa-solid fa-map-location-dot"></i> Contact</a>
+          <a href="https://sujammu.samarth.ac.in/index.php/pgportal/grievance-public/public"><i
+              class="fa-solid fa-pen"></i> Grievances</a>
+          <a href="https://www.jammuuniversity.ac.in/jobsatju/teaching-staff"><i class="fa-solid fa-user-tie"></i>
+            Career@UIET</a>
+          <a href="https://jammuuniversity.ac.in"> <i class="fa-solid fa-building-columns"></i> University of Jammu</a>
+
         </div>
         <div class="nav-column">
           <h3>Academics</h3>
-          <a href="../courses.php">Programs</a>
-          <a href="./index.php#admission">Admissions</a>
-          <a href="../document.php">Documents</a>
-          <a href="https://unijammuopac.libsys10.in">eLibrary</a>
+          <a href="../courses.php"><i class="fa-solid fa-book-open-reader"></i> Programs</a>
+
+          <a href="../document.php"><i class="fa-solid fa-folder"></i> Documents</a>
+          <a href="../results.php"><i class="fa-solid fa-graduation-cap"></i> Results</a>
+          <a href="https://unijammuopac.libsys10.in"><i class="fa-solid fa-book"></i> eLibrary</a>
         </div>
         <div class="nav-column">
           <h3>About</h3>
-          <a href="../campus.php">Campus</a>
-          <a href="../leadership.php">Leadership</a>
-          <a href="../faculty.php">Faculty</a>
-          <a href="../placements.php">Placements</a>
-          <a href="../committees.php">Committees</a>
-          <a href="https://jammuuniversity.ac.in">University of Jammu (UoJ)</a>
-          <a href="https://i4c.mha.gov.in/newsletter-manual.aspx">Cyber Security</a>
+          <a href="../campus.php"><i class="fa-solid fa-user-graduate"></i> Campus</a>
+          <a href="../leadership.php"><i class="fa-solid fa-user-tie"></i> Leadership</a>
+          <a href="../faculty.php"><i class="fa-solid fa-person-chalkboard"></i> Faculty</a>
+          <a href="../placements.php"><i class="fa-solid fa-person-circle-check"></i> Placements</a>
+          <a href="../committees.php"><i class="fa-solid fa-hand-holding-hand"></i> Committees</a>
+
+          <a href="https://i4c.mha.gov.in/newsletter-manual.aspx"><i class="fa-solid fa-shield"></i> Cyber Security</a>
         </div>
       </div>
     </div>
@@ -36,16 +42,17 @@
       <div class="footer-row footer-contact">
         <p>
           <i class="fa-solid fa-phone"></i>
-          <a href="tel:01922291102">01922291102</a>
-        </p>
-        <p>
-          <i class="fa-solid fa-at"></i>
-          <a href="mailto:uietkc@gmail.com">uietkc@gmail.com</a>
+          <a href="tel:01922291102"> 01922291102</a>
         </p>
         <p>
           <i class="fa-solid fa-globe"></i>
-          <a href="mailto:itcell.uietju@gmail.com">Web Master</a>
+          <a href="mailto:itcell.uietju@gmail.com"> Web Master</a>
         </p>
+        <p>
+          <i class="fa-solid fa-at"></i>
+          <a href="mailto:uietkc@gmail.com"> uietkc@gmail.com</a>
+        </p>
+
       </div>
     </div>
 
